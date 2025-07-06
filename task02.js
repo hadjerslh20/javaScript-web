fd10a672c15ab5ebbed2d6585b9fdbecf9844e2c
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-06 03:17:20 +0100

    Create task02.js

---
+++ task02.js
@@ -0,0 +1,20 @@
+// 1. Create variable name(firstFavNum) & store your favourite number.
+// 2. Create variable name(secondFavNum) & store your favourite number.
+// 3. Add (firstFavNum & secondFavNum).
+// 4. Substract (firstFavNum & secondFavNum).
+// 5. Multiply (firstFavNum & secondFavNum).
+// 6. Divided (firstFavNum & secondFavNum).
+// 7. Check (firstFavNum Mod secondFavNum).
+// 6. Check the power of (firstFavNum).
+
+let firstFavNum=7;
+let secondFavNum=17;
+console.log(firstFavNum+secondFavNum);
+console.log(firstFavNum-secondFavNum);
+console.log(firstFavNum*secondFavNum);
+console.log(firstFavNum/secondFavNum);
+console.log(firstFavNum%secondFavNum);
+console.log(firstFavNum**secondFavNum);
+
+
+
