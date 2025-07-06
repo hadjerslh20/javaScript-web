6f9fdbe84a27e877beb942491c468727fc7d9f99
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-06 04:18:31 +0100

    Create task04.js

---
+++ task04.js
@@ -0,0 +1,21 @@
+// 1. Create variable name(firstFavNumb) & store your favourite number.
+// 2. Create variable name(secondFavNumb) & store your second favourite number.
+// 3. Check (firstFavNumb is greater than secondFavNumb).
+// 4. Check (firstFavNumb is less than secondFavNumb).
+// 5. Check (firstFavNumb is greater than or equal  secondFavNumb).
+// 6. Check (firstFavNumb is less than or equal secondFavNumb).
+// 7. Check (firstFavNumb is  equal to secondFavNumb) using strict equality operator.
+// 8. Check (firstFavNumb is  equal to secondFavNumb) using loose equality operator.
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
+// 10. Check  (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
+
+let firstFavNumb =7;
+let secondFavNumb=17;
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
