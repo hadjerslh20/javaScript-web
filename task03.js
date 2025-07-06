8c9334170c189cafa9f8b349c1e97de5993183d4
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-06 03:33:33 +0100

    Create task03.js

---
+++ task03.js
@@ -0,0 +1,14 @@
+// 1. Create variable name(isJsProgrammingLanguage) & store true as a value.
+// 2. Create variable name(isJsHard) & store false as a value.
+// 3. Create variable name(favNumb) & store your favourite number inside.
+// 4. Now Add favNumb with the value of undefined.
+
+let isJsProgrammingLanguage=true;
+let isJsHard=false;
+let favNumb=7;
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb+undefined);
+
+
+
