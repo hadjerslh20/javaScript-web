c4eb9767ce291351a9312efb85ed9c62a81d8903
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-08 02:36:29 +0100

    Create task11.js

---
+++ task11.js
@@ -0,0 +1,14 @@
+// 1. Create array name (favSingers) store 3 favourite singers .
+// 2. Log the first singer in that array .
+// 3. Create array name (favNumbers) & store 4 fav Numbers .
+// 4. Create array name(mixedArr) store ["string",["otherarray"],123,true]
+// 5. Now Access each item in that array by using [] notation .
+
+let favSingers=["Hassni","INNA","Mouard"];
+console.log(favSingers[0]);
+let favNumbers =[7,17,10,27];
+let mixedArr = ["string",favNumbers,123,true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
